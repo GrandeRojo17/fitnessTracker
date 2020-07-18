2019606b5f41393ba5eb34ac0a9494cd503f65ce
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-07-18 16:29:46 -0500

    Looking toward notes and designing it all up

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//still working on functionality 
+//have to look towards last few assignmenets and look at the 
+// documents I drew up
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -16,3 +19,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 const mongodb_uri = process.env.mongodbUri || "mongodb://loclalhost/workouts";
+mondgoose.connect(mongodb_uri,{
+    useNewUrlParser:true,
+    useFindAndMod:false;
+    app.use(routes);
+    app.use(htmlRoutes);
+
+
+});
+app.listen(PORT,() => {
+
+    console.log(`App running on port ${PORT}`; 
+});
